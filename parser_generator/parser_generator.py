d5a8443d5286dac2c1da4624c2f33ef304f4cc99
Basem-Gaber <bmgaberah@hotmail.com>
2021-05-16 22:29:42 +0200

    Finished parsing_table

--- parser_generator/parser_generator.py
+++ parser_generator/parser_generator.py
@@ -6,6 +6,7 @@ class ParserGenerator:
         self.grammarHndlr = grammarHndlr
         self.first = {}
         self.follow = {}
+        self.parsing_table = None
 
     def get_me_start_symbol(self):
         grammar = self.grammarHndlr.grammar
@@ -43,8 +44,8 @@ class ParserGenerator:
         for non in grammar:
             self.compute_First(non)
 
-        print(self.first)
-        return
+        # print(self.first)
+        return self.first
 
     def follow_sets(self):
         grammar = self.grammarHndlr.grammar
@@ -96,15 +97,38 @@ class ParserGenerator:
                             self.follow[item].add('$')
                         self.follow[item].update(set_to_add)
 
-        print(self.follow)
-        return
+        # print(self.follow)
+        return self.follow
 
     def generate_parsing_table(self):
+        grammar = self.grammarHndlr.grammar
+        terminals = self.grammarHndlr.terminals
+        non_terminals = self.grammarHndlr.non_terminals
+
         first_sets = self.first_sets()
         follow_sets = self.follow_sets()
 
-        self.parsing_table = None
+        M = {}
+        for non in non_terminals:
+            M[non] = {}
 
+        for symbol in grammar:
+            productions = grammar[symbol]
+            for production in productions:
+                first_alpha = self.first[production[0]]
+                for a in first_alpha:
+                    if a in terminals and a != '\\L':
+                        M[symbol][a] = production
+                    if a == '\\L':
+                        follow_A = self.follow[symbol]
+                        if '$' in follow_A:
+                            M[symbol]['$'] = production
+                        for fol_a in follow_A:
+                            if fol_a in terminals:
+                                M[symbol][fol_a] = production
+        self.parsing_table = M
+        for key, value in M.items():
+            print(key, ' : ', value)
         return self.parsing_table
 
     def generate(self):
